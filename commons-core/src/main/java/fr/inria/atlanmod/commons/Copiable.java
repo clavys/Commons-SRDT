6c17d40bb06a58621116ad4cd58eccef350307f3
Yoann Vernageau <y.vernageau@gmail.com>
2017-09-03 23:28:44 +0200

    add `Copiable` and `Copier` interfaces

---
+++ commons-core/src/main/java/fr/inria/atlanmod/commons/Copiable.java
@@ -0,0 +1,19 @@
+package fr.inria.atlanmod.commons;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+/**
+ * An object that can be copied to another.
+ *
+ * @param <T> the type of the copied object
+ */
+@ParametersAreNonnullByDefault
+public interface Copiable<T> {
+
+    /**
+     * Copies all the contents from this object to the {@code target}.
+     *
+     * @param target the object where to store the copied content
+     */
+    void copyTo(T target);
+}
