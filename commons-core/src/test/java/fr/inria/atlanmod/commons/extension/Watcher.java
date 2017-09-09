5f61ac5200359157c3c4cc016ab78a3f83a3d43a
Yoann Vernageau <y.vernageau@gmail.com>
2017-09-09 18:56:48 +0200

    add memory management during tests

--- commons-core/src/test/java/fr/inria/atlanmod/commons/extension/Watcher.java
+++ commons-core/src/test/java/fr/inria/atlanmod/commons/extension/Watcher.java
@@ -23,6 +23,11 @@ import org.junit.runners.model.MultipleFailureException;
 import java.util.Arrays;
 import java.util.Collections;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static fr.inria.atlanmod.commons.Preconditions.checkState;
 import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
@@ -59,22 +64,27 @@ public class Watcher extends TestWatcher {
     /**
      * The message to display a failed test with errors.
      */
-    private static final String MESSAGE_FAILURE_ERRORS = MESSAGE_FAILURE + " with error(s):";
+    private static final String MESSAGE_FAILURE_ERRORS = MESSAGE_FAILURE + " with error(s)";
 
     /**
      * The message to display a skipped test.
      */
     private static final String MESSAGE_SKIP = "--- Skipped";
 
+    /**
+     * The used memory before starting the test memthod, in bytes.
+     */
+    private MemoryUsage memoryAtStart;
+
     @Override
     protected void succeeded(Description description) {
-        LOG_TEST.info(MESSAGE_SUCCESS);
+        LOG_TEST.info(MESSAGE_SUCCESS + usedMemory());
     }
 
     @Override
     protected void failed(Throwable e, Description description) {
         if (isNull(e)) {
-            LOG_TEST.warn(MESSAGE_FAILURE);
+            LOG_TEST.warn(MESSAGE_FAILURE + usedMemory());
         }
         else if (AssertionError.class.isInstance(e) && nonNull(e.getMessage())) {
             Arrays.stream(e.getMessage().split("\n"))
@@ -82,10 +92,10 @@ public class Watcher extends TestWatcher {
                     .forEach(LOG_TEST::warn);
 
             LOG_TEST.warn(Strings.EMPTY);
-            LOG_TEST.warn(MESSAGE_FAILURE);
+            LOG_TEST.warn(MESSAGE_FAILURE + usedMemory());
         }
         else {
-            LOG_TEST.error(MESSAGE_FAILURE_ERRORS);
+            LOG_TEST.error(MESSAGE_FAILURE_ERRORS + usedMemory() + ':');
 
             (MultipleFailureException.class.isInstance(e)
                     ? MultipleFailureException.class.cast(e).getFailures()
@@ -110,10 +120,91 @@ public class Watcher extends TestWatcher {
     protected void starting(Description description) {
         LOG_VOID.info(Strings.EMPTY);
         LOG_TEST.info(MESSAGE_RUNNING + ": {0}", description.getMethodName());
+
+        memoryAtStart = new MemoryUsage();
     }
 
     @Override
     protected void finished(Description description) {
         LOG_VOID.info(Strings.EMPTY);
+
+        // Runs the GC after each test
+        Runtime.getRuntime().gc();
+    }
+
+    /**
+     * Formats the memory usage of this test.
+     *
+     * @return a formatted string of the memory used during this test
+     */
+    @Nonnull
+    private String usedMemory() {
+        checkState(nonNull(memoryAtStart), "memoryAtStart has not bben initialized");
+
+        return memoryAtStart.formatDiff(new MemoryUsage());
+    }
+
+    @ParametersAreNonnullByDefault
+    private static final class MemoryUsage {
+
+        /**
+         * The total amount of memory in the Java virtual machine, in bytes.
+         */
+        @Nonnegative
+        private final long total;
+
+        /**
+         * The amount of free memory in the Java Virtual Machine, in bytes.
+         */
+        @Nonnegative
+        private final long free;
+
+        /**
+         * Constructs a new {@code MemoryUsage}.
+         */
+        public MemoryUsage() {
+            Runtime runtime = Runtime.getRuntime();
+
+            this.total = runtime.totalMemory();
+            this.free = runtime.freeMemory();
+        }
+
+        /**
+         * Converts the {@code bytes} value to another unit (kilobytes, megabytes,...).
+         */
+        private static long convertUnit(long bytes) {
+            return bytes / 1024 / 1024;
+        }
+
+        /**
+         * Returns the total amount of memory in the Java virtual machine, in bytes.
+         */
+        @Nonnegative
+        public long total() {
+            return total;
+        }
+
+        /**
+         * Returns the amount of free memory in the Java Virtual Machine, in bytes.
+         */
+        @Nonnegative
+        public long free() {
+            return free;
+        }
+
+        /**
+         * Returns the amount of used memory in the Java Virtual Machine, in bytes.
+         */
+        @Nonnegative
+        public long used() {
+            return total() - free();
+        }
+
+        @Nonnull
+        public String formatDiff(MemoryUsage after) {
+            return String.format(" [%d/%d MB] ",
+                    convertUnit(after.used() - used()),
+                    convertUnit(after.total()));
+        }
     }
 }
