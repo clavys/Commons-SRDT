bf41bd4a499713adaaeb16328872b64c8971ad7a
Yoann Vernageau <y.vernageau@gmail.com>
2017-09-09 20:47:25 +0200

    add more details for memory usage

--- commons-core/src/test/java/fr/inria/atlanmod/commons/extension/Watcher.java
+++ commons-core/src/test/java/fr/inria/atlanmod/commons/extension/Watcher.java
@@ -37,6 +37,11 @@ import static java.util.Objects.nonNull;
 public class Watcher extends TestWatcher {
 
     /**
+     * {@code true} if the memory usage must be logged during testing.
+     */
+    private static final boolean MEMORY_USAGE = true;
+
+    /**
      * The special logger without timestamp.
      */
     private static final Logger LOG_TEST = Log.forName("test");
@@ -78,13 +83,13 @@ public class Watcher extends TestWatcher {
 
     @Override
     protected void succeeded(Description description) {
-        LOG_TEST.info(MESSAGE_SUCCESS + usedMemory());
+        LOG_TEST.info(MESSAGE_SUCCESS + memoryUsage());
     }
 
     @Override
     protected void failed(Throwable e, Description description) {
         if (isNull(e)) {
-            LOG_TEST.warn(MESSAGE_FAILURE + usedMemory());
+            LOG_TEST.warn(MESSAGE_FAILURE + memoryUsage());
         }
         else if (AssertionError.class.isInstance(e) && nonNull(e.getMessage())) {
             Arrays.stream(e.getMessage().split("\n"))
@@ -92,10 +97,10 @@ public class Watcher extends TestWatcher {
                     .forEach(LOG_TEST::warn);
 
             LOG_TEST.warn(Strings.EMPTY);
-            LOG_TEST.warn(MESSAGE_FAILURE + usedMemory());
+            LOG_TEST.warn(MESSAGE_FAILURE + memoryUsage());
         }
         else {
-            LOG_TEST.error(MESSAGE_FAILURE_ERRORS + usedMemory() + ':');
+            LOG_TEST.error(MESSAGE_FAILURE_ERRORS + memoryUsage() + ':');
 
             (MultipleFailureException.class.isInstance(e)
                     ? MultipleFailureException.class.cast(e).getFailures()
@@ -121,7 +126,9 @@ public class Watcher extends TestWatcher {
         LOG_VOID.info(Strings.EMPTY);
         LOG_TEST.info(MESSAGE_RUNNING + ": {0}", description.getMethodName());
 
-        memoryAtStart = new MemoryUsage();
+        if (MEMORY_USAGE) {
+            memoryAtStart = new MemoryUsage();
+        }
     }
 
     @Override
@@ -138,10 +145,13 @@ public class Watcher extends TestWatcher {
      * @return a formatted string of the memory used during this test
      */
     @Nonnull
-    private String usedMemory() {
-        checkState(nonNull(memoryAtStart), "memoryAtStart has not bben initialized");
+    private String memoryUsage() {
+        if (MEMORY_USAGE) {
+            checkState(nonNull(memoryAtStart), "memoryAtStart has not bben initialized");
+            return " [" + MemoryUsage.Diff.between(memoryAtStart, new MemoryUsage()).toString() + ']';
+        }
 
-        return memoryAtStart.formatDiff(new MemoryUsage());
+        return Strings.EMPTY;
     }
 
     @ParametersAreNonnullByDefault
@@ -200,11 +210,52 @@ public class Watcher extends TestWatcher {
             return total() - free();
         }
 
-        @Nonnull
-        public String formatDiff(MemoryUsage after) {
-            return String.format(" [%d/%d MB] ",
-                    convertUnit(after.used() - used()),
-                    convertUnit(after.total()));
+        @ParametersAreNonnullByDefault
+        public static final class Diff {
+
+            @Nonnull
+            private final MemoryUsage start;
+
+            @Nonnull
+            private final MemoryUsage end;
+
+            /**
+             * Creates a new {@code Diff}.
+             */
+            private Diff(MemoryUsage start, MemoryUsage end) {
+                this.start = start;
+                this.end = end;
+            }
+
+            /**
+             * Creates a {@code Diff} reprensenting the difference between two memory usages.
+             */
+            public static Diff between(MemoryUsage start, MemoryUsage end) {
+                return new Diff(start, end);
+            }
+
+            public long total() {
+                return end.total() - start.total();
+            }
+
+            public long free() {
+                return end.free() - start.free();
+            }
+
+            public long used() {
+                return end.used() - start.used();
+            }
+
+            @Override
+            public String toString() {
+                final long usedDiff = convertUnit(used());
+                final long totalDiff = convertUnit(total());
+
+                return String.format("%s / %s%s MB",
+                        usedDiff < 0 ? "<0" : usedDiff,
+                        convertUnit(end.total()),
+                        totalDiff == 0 ? Strings.EMPTY : ("(" + (totalDiff > 0 ? "+" + totalDiff : totalDiff) + ")"));
+            }
         }
     }
 }
