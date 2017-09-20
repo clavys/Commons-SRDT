eb9b5471a9955d2190d08c652f972731fd04ef95
Yoann Vernageau <y.vernageau@gmail.com>
2017-09-20 18:38:50 +0200

    add utility methods about Java primitives

---
+++ commons-core/src/main/java/fr/inria/atlanmod/commons/primitive/Primitives.java
@@ -0,0 +1,73 @@
+package fr.inria.atlanmod.commons.primitive;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Static utility methods related to Java primitive types.
+ */
+public final class Primitives {
+
+    /**
+     * A set of primitive types.
+     */
+    @Nonnull
+    private static final Set<Class<?>> PRIMITIVE_TYPES = new HashSet<>();
+
+    static {
+        PRIMITIVE_TYPES.add(Boolean.class);
+        PRIMITIVE_TYPES.add(Character.class);
+        PRIMITIVE_TYPES.add(Byte.class);
+        PRIMITIVE_TYPES.add(Short.class);
+        PRIMITIVE_TYPES.add(Integer.class);
+        PRIMITIVE_TYPES.add(Long.class);
+        PRIMITIVE_TYPES.add(Float.class);
+        PRIMITIVE_TYPES.add(Double.class);
+        PRIMITIVE_TYPES.add(Void.class);
+    }
+
+    /**
+     * This class should not be instantiated.
+     *
+     * @throws IllegalStateException every time
+     */
+    private Primitives() {
+        throw new IllegalStateException("This class should not be instantiated");
+    }
+
+    /**
+     * Returns {@code true} if the {@code type} is considered as a Java primitive type, or its boxed equivalent, or a
+     * {@link String} type.
+     *
+     * @param type the type to check
+     *
+     * @return {@code true} if the {@code type} is considered as a primitive type.
+     */
+    public static boolean isPrimitiveOrString(Class<?> type) {
+        return isPrimitive(type) || type == String.class || isBoxed(type);
+    }
+
+    /**
+     * Returns {@code true} if the {@code type} is a boxed Java primitive type.
+     *
+     * @param type the type to check
+     *
+     * @return {@code true} if the {@code type} is a boxed Java primitive type
+     */
+    public static boolean isBoxed(Class<?> type) {
+        return PRIMITIVE_TYPES.contains(type);
+    }
+
+    /**
+     * Returns {@code true} if the {@code type} is a Java primitive type.
+     *
+     * @param type the type to check
+     *
+     * @return {@code true} if the {@code type} is a Java primitive type
+     */
+    public static boolean isPrimitive(Class<?> type) {
+        return type.isPrimitive();
+    }
+}
