63a144f0dafadf7baec9ffd02ba24bf97b6d5722
Yoann Vernageau <y.vernageau@gmail.com>
2017-10-05 05:31:11 +0200

    change license to EPL v2.0

--- commons-core/src/main/java/fr/inria/atlanmod/commons/collect/MoreStreams.java
+++ commons-core/src/main/java/fr/inria/atlanmod/commons/collect/MoreStreams.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2017 Atlanmod, Inria, LS2N, and IMT Nantes.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v2.0 which accompanies
+ * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
+ */
+
 package fr.inria.atlanmod.commons.collect;
 
 import fr.inria.atlanmod.commons.annotation.Static;
