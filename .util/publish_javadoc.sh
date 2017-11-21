8ea9d7f656a5c135cb69695dd5e4f64e52c3cc3b
Yoann Vernageau <y.vernageau@gmail.com>
2017-11-21 15:15:42 +0100

    update `public_javadoc.sh`

--- .util/publish_javadoc.sh
+++ .util/publish_javadoc.sh
@@ -7,7 +7,7 @@ OS="linux"
 
 API_DIR="doc"
 ROOT_API_DIR="releases/snapshot"
-TEMP_DIR="$HOME/$API_DIR"
+TEMP_DIR="${HOME}/$API_DIR"
 
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping Javadoc publication: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
@@ -31,8 +31,9 @@ else
 
     echo -e "Copying Javadoc..."
 
+    mkdir -p ${TEMP_DIR}
     cp -rf target/site/apidocs ${TEMP_DIR}
-    cd $HOME
+    cd ${HOME}
 
     if ! [ -d "gh-pages" ]; then
         echo -e "Cloning 'gh-pages' branch..."
@@ -49,7 +50,7 @@ else
     mkdir -p ${ROOT_API_DIR}
     cd ${ROOT_API_DIR}
 
-    if [ -d "$API_DIR" ]; then
+    if [ -d "${API_DIR}" ]; then
         git rm --quiet -rf ${API_DIR}
     fi
 
