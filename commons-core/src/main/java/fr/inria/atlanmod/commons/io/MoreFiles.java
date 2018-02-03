346dc993d963a1072e49bd7984560a6ad54279ee
Yoann Vernageau <y.vernageau@gmail.com>
2018-02-03 16:11:51 +0100

    add `MoreFiles` methods with `File`

--- commons-core/src/main/java/fr/inria/atlanmod/commons/io/MoreFiles.java
+++ commons-core/src/main/java/fr/inria/atlanmod/commons/io/MoreFiles.java
@@ -36,36 +36,68 @@ public final class MoreFiles {
     }
 
     /**
-     * Returns the file extension for the given {@code file} name, or the empty string if the file has no extension. The
-     * result does not include the '{@code .}'.
+     * Returns the file extension for the given {@code file}, or the empty string if the file has no extension.
+     * The result does not include the '{@code .}'.
      *
-     * @param file the file name
+     * @param file the file
      *
-     * @return the filename extension
+     * @return the file extension
      */
     @Nonnull
-    public static String fileExtension(String file) {
+    public static String fileExtension(File file) {
         checkNotNull(file, "file");
 
-        String fileName = new File(file).getName();
+        String fileName = file.getName();
         int dotIndex = fileName.lastIndexOf(DOT);
         return (dotIndex == -1) ? Strings.EMPTY : fileName.substring(dotIndex + 1);
     }
 
     /**
-     * Returns the {@code file} name without its file extension or path. This is similar to the {@code basename} unix
-     * command. The result does not include the '{@code .}'.
+     * Returns the file extension for the given file name, or the empty string if the fullName has no extension.
+     * The result does not include the '{@code .}'.
      *
-     * @param file the file name
+     * @param fullName the file name
      *
-     * @return the filename without its extension
+     * @return the fullName extension
      */
     @Nonnull
-    public static String nameWithoutExtension(String file) {
+    public static String fileExtension(String fullName) {
+        checkNotNull(fullName, "fullName");
+
+        return fileExtension(new File(fullName));
+    }
+
+    /**
+     * Returns the {@code file} name without its file extension or path.
+     * This is similar to the {@code basename} unix command.
+     * The result does not include the '{@code .}'.
+     *
+     * @param file the file
+     *
+     * @return the file name without its extension
+     */
+    @Nonnull
+    public static String nameWithoutExtension(File file) {
         checkNotNull(file, "file");
 
-        String fileName = new File(file).getName();
+        String fileName = file.getName();
         int dotIndex = fileName.lastIndexOf(DOT);
         return (dotIndex == -1) ? fileName : fileName.substring(0, dotIndex);
+    }
+
+    /**
+     * Returns the file name without its fullName extension or path.
+     * This is similar to the {@code basename} unix command.
+     * The result does not include the '{@code .}'.
+     *
+     * @param fullName the file name
+     *
+     * @return the fullName name without its extension
+     */
+    @Nonnull
+    public static String nameWithoutExtension(String fullName) {
+        checkNotNull(fullName, "fullName");
+
+        return nameWithoutExtension(new File(fullName));
     }
 }
