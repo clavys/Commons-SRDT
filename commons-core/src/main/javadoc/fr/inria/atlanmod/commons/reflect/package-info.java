71d5453dac2ebf39f3f8be7c4f8336cad731e75c
Yoann Vernageau <y.vernageau@gmail.com>
2018-02-16 15:09:44 +0100

    add javadoc

---
+++ commons-core/src/main/javadoc/fr/inria/atlanmod/commons/reflect/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2017-2018 Atlanmod, Inria, LS2N, and IMT Nantes.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v2.0 which accompanies
+ * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
+ */
+
+/**
+ * Provides classes and methods to ease the use of reflection.
+ */
+
+package fr.inria.atlanmod.commons.reflect;
