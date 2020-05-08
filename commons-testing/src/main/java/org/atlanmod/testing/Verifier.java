dd0e7a6a768d94e9720bee66c56805693a097cd0
sunye <gerson.sunye@gmail.com>
2020-05-08 21:11:07 +0200

    Add new module for testing utilities and helpers

---
+++ commons-testing/src/main/java/org/atlanmod/testing/Verifier.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 Atlanmod.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v2.0 which accompanies
+ * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
+ */
+package org.atlanmod.testing;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class Verifier {
+
+    public static EqualsPredicate verifyEqualsOf(Class klass) {
+        return new EqualsPredicate(klass);
+    }
+
+
+}
