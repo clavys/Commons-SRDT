fa10d00171a9fdaf1406eb68d4ef2fd5129e140a
Gerson Sunyé <sunye@atlanmod.org>
2021-07-12 10:14:35 +0200

    Add class to deal with a set of bits

--- commons-core/src/main/java/org/atlanmod/commons/collect/Path.java
+++ commons-core/src/main/java/org/atlanmod/commons/collect/Path.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021 Naomod.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v2.0 which accompanies
+ * this distribution, and is available at https://www.eclipse.org/legal/epl-2.0/
+ */
+
 package org.atlanmod.commons.collect;
 
 import javax.annotation.ParametersAreNonnullByDefault;
